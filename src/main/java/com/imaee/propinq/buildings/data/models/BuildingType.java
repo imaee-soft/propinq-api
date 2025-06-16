04c1fe99636457d784f774f0c4e16aff1efbaedd
Martín Jesús Crosetto <martincrosetto312@gmail.com>
2025-06-15 23:15:56 -0300

    Ref: changed annotations in some entities, and added an data.sql to inject data into the schema

--- src/main/java/com/imaee/propinq/buildings/data/models/BuildingType.java
+++ src/main/java/com/imaee/propinq/buildings/data/models/BuildingType.java
@@ -2,8 +2,10 @@ package com.imaee.propinq.buildings.data.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity(name="building_types")
@@ -17,5 +19,8 @@ public class BuildingType {
 
     @NonNull
     private String name;
+
+    @OneToMany
+    private List<Building> buildings;
 
 }
