1fdc1cf1b7b198f6c2487e65bdcd55604e9f0801
Ignacio Gonzalez <55899890+NachoG13@users.noreply.github.com>
2025-06-21 16:06:57 -0300

    [PROPINQ-8]: Dockerización (#4)

--- docker-entrypoint.dev.sh
+++ docker-entrypoint.dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Dspring.devtools.restart.enabled=true -Dspring.devtools.livereload.enabled=true" &
+./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-Dspring.devtools.restart.enabled=true -Dspring.devtools.livereload.enabled=true" &
 while inotifywait -r -e modify,create,delete src/; do
     echo "File changes detected, recompiling..."
-    mvn compile
+   ./mvnw compile
 done
