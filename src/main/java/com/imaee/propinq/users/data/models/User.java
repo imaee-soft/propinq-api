fae79ffde1c600e3a40af6c79fcdf06d4565c182
Martín Jesús Crosetto <martincrosetto312@gmail.com>
2025-06-23 18:47:50 -0300

    data.sql works!

--- src/main/java/com/imaee/propinq/users/data/models/User.java
+++ src/main/java/com/imaee/propinq/users/data/models/User.java
@@ -1,11 +1,26 @@
 package com.imaee.propinq.users.data.models;
 
+import com.imaee.propinq.buildings.data.models.Building;
+import com.imaee.propinq.properties.data.models.Property;
 import com.imaee.propinq.users.data.pipes.PhoneNumberConverter;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Convert;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
 import com.imaee.propinq.users.data.enums.Role;
 
 
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 @Entity(name="users")
@@ -16,7 +31,7 @@ import java.util.UUID;
 public class User {
 
     @Id
-    private final UUID userId = UUID.randomUUID();
+    private UUID userId = UUID.randomUUID();
 
     @NonNull
     @Column(unique = true)
@@ -38,9 +53,16 @@ public class User {
     @Convert(converter = PhoneNumberConverter.class)
     private String phoneNumber;
 
+    @OneToMany
+    private List<Building> buildings = Collections.emptyList();
+
+    @OneToMany
+    private List<Property> properties = Collections.emptyList();
+
     @Enumerated(EnumType.STRING)
     @Builder.Default
     private Role role = Role.TENANT;
 
     private Boolean deleted = false;
+
 }
