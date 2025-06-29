29f699a0deda0d0e2ce29fd51103de1f51aadbda
Ignacio Gonzalez <ignacio.rover@gmail.com>
2025-06-29 17:44:56 -0300

    add: Permissions & map permissions to role

---
+++ src/main/java/com/imaee/propinq/users/data/enums/Role.java
@@ -0,0 +1,28 @@
+package com.imaee.propinq.users.data.enums;
+
+import java.util.Set;
+import java.util.List;
+
+public enum Role {
+    TENANT,
+    OWNER,
+    ADMIN;
+
+    public String getRoleName() {
+        return this.name();
+    }
+
+    public Set<Permission> getPermissions() {
+        return RolePermissionMap.getPermissions(this);
+    }
+
+    public List<String> getAuthorities() {
+        return getPermissions().stream()
+                .map(Permission::name)
+                .toList();
+    }
+
+    public boolean hasPermission(Permission permission) {
+        return getPermissions().contains(permission);
+    }
+}
