41147e1e8de6c4b66ae45e156e33f61de3418e7b
frosskiel <frosskiel@gmail.com>
2025-07-01 22:05:10 -0300

    ref: remove * imports

--- src/main/java/com/imaee/propinq/neighborhood/data/models/Neighborhood.java
+++ src/main/java/com/imaee/propinq/neighborhood/data/models/Neighborhood.java
@@ -1,8 +1,18 @@
 package com.imaee.propinq.neighborhood.data.models;
 
 import com.imaee.propinq.shared.data.models.Locatable;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.util.UUID;
 
