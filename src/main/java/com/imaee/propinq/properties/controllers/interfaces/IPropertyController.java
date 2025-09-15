fee338c135e9ab5ce17a7390ee7407d722239b87
Ignacio Gonzalez <ignacio.rover@gmail.com>
2025-09-14 23:18:05 -0300

    feat: filtar viviendas. todo: change response of filterProperties on PopertyService

--- src/main/java/com/imaee/propinq/properties/controllers/interfaces/IPropertyController.java
+++ src/main/java/com/imaee/propinq/properties/controllers/interfaces/IPropertyController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
+
 import java.util.List;
 import java.util.UUID;
 
