9d310a5b9ce9e9d6738859d91ed5a5fd27f427c1
Ezequiel Sosa <ezequielsosa423@gmail.com>
2025-10-10 22:19:53 -0300

    Merge branch 'dev' into PROPINQ-56

--- src/main/java/com/imaee/propinq/properties/services/usecases/interfaces/ICreatePropertyTypeUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.imaee.propinq.properties.services.usecases.interfaces;
-
-import com.imaee.propinq.properties.controllers.requests.PropertyTypeRequest;
-import com.imaee.propinq.properties.controllers.responses.PropertyTypeResponse;
-
-public interface ICreatePropertyTypeUseCase {
-    PropertyTypeResponse createPropertyType(PropertyTypeRequest propertyTypeRequest);
-}
